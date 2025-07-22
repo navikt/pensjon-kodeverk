20175dd96679e576935f5f4902c60f36acb7bdc8
Morten Normann <morten.normann@nav.no>
2025-07-22 09:40:18 +0200

    Initiell commit

---
+++ src/main/kotlin/no/nav/pensjon_kodeverk/PensjonKodeverkApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon_kodeverk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PensjonKodeverkApplication
+
+fun main(args: Array<String>) {
+    runApplication<PensjonKodeverkApplication>(*args)
+}
